49717c35dd199a12a1dee66449d9f3ec7aa91986
alisonsantosofc <alisonsantoso22@gmail.com>
2023-06-15 03:15:10 -0300

    build: initialize project structure

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { ThemeProvider } from "styled-components";
+
+import { useDarkMode } from "./hooks/useDarkMode";
+
+import { GlobalStyles } from "./styles/global";
+import { darkTheme, lightTheme } from "./styles/themes";
+import { Header } from "./components/Header";
+import { Home } from "./views/Home";
+
+export function App() {
+  // const { darkMode } = useDarkMode();
+
+  const darkMode = false;
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Header />
+
+      <main>
+        <Home />
+      </main>
+
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+}
