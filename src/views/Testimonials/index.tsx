07c6871ae6ea558271f30bd68856d4b9c1bb231a
alisonsantosofc <alisonsantoso22@gmail.com>
2023-06-27 23:03:24 -0300

    feat: creating testimonials

---
+++ src/views/Testimonials/index.tsx
@@ -0,0 +1,14 @@
+import { StyledTestimonials } from "./styles";
+
+export default function Testimonials() {
+  return (
+    <StyledTestimonials className="section" id="testimonials">
+      <h2 className="section-title">Testimonials</h2>
+      <span className="section-subtitle">O que eu ofereço</span>
+
+      <div className="services-container container grid">
+        
+      </div>
+    </StyledTestimonials>
+  )
+}
--- src/views/Testimonials/styles.ts
+++ src/views/Testimonials/styles.ts
@@ -1,5 +1,5 @@
 import { styled } from "styled-components";
 
-export const StyledServices = styled.section`
+export const StyledTestimonials = styled.section`
   
 `;
