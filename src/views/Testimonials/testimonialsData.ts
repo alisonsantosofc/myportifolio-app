a2b79ebb9a0e9c7ddb25067a03b88c1886c5aaa8
alisonsantosofc <alisonsantoso22@gmail.com>
2023-06-27 23:49:15 -0300

    feat: creating testimonials

---
+++ src/views/Testimonials/testimonialsData.ts
@@ -0,0 +1,24 @@
+import image1 from "../../assets/testimonials/image1.jpeg";
+import image2 from "../../assets/testimonials/image2.jpeg";
+import image3 from "../../assets/testimonials/image3.jpeg";
+
+export const testimonialsData = [
+  {
+    id: 1,
+    image: image1,
+    title: "Marcio Clume",
+    description: "Ele entendeu perfeitamente as nossas necessidades e conseguiu transformar nossas ideias em soluções funcionais e eficientes.",
+  },
+  {
+    id: 2,
+    image: image2,
+    title: "Charlys Torres",
+    description: "Sua habilidade em encontrar soluções inovadoras para os desafios técnicos que enfrentamos foi excepcional.",
+  },
+  {
+    id: 3,
+    image: image3,
+    title: "Wayne Deyvson",
+    description: "Eu sempre soube que você tinha um talento especial, mas ver como você se tornou um verdadeiro especialista é algo extraordinário.",
+  }
+];
